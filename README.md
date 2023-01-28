54005a54db4795069ec8e283a6b28c84c0561ab0
Rodrigo <101999518+rdGxd@users.noreply.github.com>
2023-01-28 19:12:59 -0300

    Ajustado

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Countdown
+
+**Requisitos para o desafio:**
+
+- Para concluir o desafio você deve cumprir alguns requisitos:
+  1. A página deve ter um **countdown timer** com uma contagem regressiva para o tempo de lançamento.
+  2. Adicione um **botão** com o texto "inscreva-se".
--- main.js
+++ main.js
@@ -5,11 +5,18 @@ const minuto = 60 * segundo;
 const hora = 60 * minuto;
 const dia = 24 * hora;
 
-function updateTimer() {
-  dataFinal = Date.parse("Feb 25 2023 07:00:00 GMT-3");
-  dataAtual = new Date();
-  let diff = dataFinal - dataAtual;
 
+function updateTimer() {
+  // Formato da Data e Hora
+  dataFinal = Date.parse("Feb 25 2023 07:00:00 GMT-3");
+
+  // Pega a data atual do navegador
+  dataAtual = new Date();
+
+  // Calcula a diferença entre a data final do projeto e a data atual do navegador
+  let diff = dataFinal - dataAtual;
+  
+  // Calculo de Dias, Horas, Minutos e Segundos
   const dias = Math.floor(diff / dia);
   diff -= dias * dia;
   const horas = Math.floor(diff / hora);
@@ -20,10 +27,12 @@ function updateTimer() {
 
   tempo.innerHTML = `${dias} : ${horas} : ${minutos} : ${segundos}`;
 
+// Mensagem quando a contagem acabar
   if (dataAtual > dataFinal) {
     tempo.innerHTML = "Tempo esgotado";
     clearInterval(intervalID);
   }
 }
 
+// Chama a função a cada 1s
 const intervalID = setInterval("updateTimer()", 1000);
